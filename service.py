5a7f5ee7ddf80daae78f1e35aebd007481c4e3e8
Balint Csergo <balint.csergo@emarsys.com>
2020-04-19 14:51:28 +0200

    switch from pystemd to dbus

--- service.py
+++ service.py
@@ -1,15 +1,19 @@
-from pystemd.systemd1 import Unit
+import dbus
 
 
 class ForgeService:
     def __init__(self):
-        self.unit = Unit(b"minecraft.service", _autoload=True)
+        self.sysbus = dbus.SystemBus()
+        self.systemd1 = self.sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+        self.manager = dbus.Interface(self.systemd1, 'org.freedesktop.systemd1.Manager')
+        self.service = self.sysbus.get_object('org.freedesktop.systemd1', object_path=self.manager.GetUnit('minecraft.service'))
+        self.interface = dbus.Interface(self.service, dbus_interface='org.freedesktop.DBus.Properties')
 
     def status(self) -> str:
-        return self.unit.Unit.ActiveState.decode("utf-8") 
+        return self.interface.Get('org.freedesktop.systemd1.Unit', 'ActiveState')
 
     def start(self):
-        self.unit.Unit.Start("replace")
+        self.manager.StartUnit('minecraft.service', 'fail')
 
     def stop(self):
-        self.unit.Unit.Stop("replace")
+        self.manager.StopUnit('minecraft.service', 'fail')
